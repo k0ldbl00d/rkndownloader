4fa92cfeac61865fa844ed945abe4b2610a16241
Kirill Shmakov <k0ldbl00d@gmail.com>
2022-02-04 15:25:53 +0400

    Формирование скрипта для Mikrotik RouterOS

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,5 @@ WORKDIR /app
 RUN mkdir -p /app/data
 ADD src/*.php /app/
 COPY docker-entrypoint.sh /app
-RUN apk add libxml2-dev && docker-php-ext-install soap
+RUN apk add libxml2-dev libzip-dev && docker-php-ext-install soap zip
 ENTRYPOINT [ "sh", "docker-entrypoint.sh" ]
--- src/parser.php
+++ src/parser.php
@@ -12,10 +12,20 @@ class Parser {
         if(preg_match_all("/([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(\/[0-9]{1,3})?)/", $xml, $matches)) {
             $ips = array_unique($matches[1]);
             file_put_contents( "./data/social-ips.txt", implode( "\n", $ips ) );
+            $this->mkMikrotikScript($ips);
             logw("Найдено IP-адресов: " . count($ips));
         } else {
             logw("В файле не найдены IP-адреса");
         }
 
+    }
+
+    function mkMikrotikScript($ips, $listname="social-sites") {
+
+        $lines = array_map(function($ip) use ($listname) {
+            return "add list={$listname} address={$ip} comment=\"Socially significant resources\"";
+        }, $ips);
+        $script = "/ip firewall address-list\n\nremove [ find where list={$listname} ]\n\n" . implode("\n",$lines) . "\n";
+        file_put_contents("./data/mikrotik-social.rsc", $script);
     }
 }
