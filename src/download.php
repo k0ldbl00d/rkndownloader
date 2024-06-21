37723b93b5811caf771e6094b1364c648a19acf2
Kirill Shmakov <k0ldbl00d@gmail.com>
2024-06-21 13:24:42 +0400

    Try increase socket timeout and set UA

--- src/download.php
+++ src/download.php
@@ -5,6 +5,9 @@ require_once("model.php");
 require_once("downloader.php");
 require_once("parser.php");
 
+ini_set('default_socket_timeout', 600);
+ini_set('user_agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36');
+
 $dl = new Downloader();
 
 if($dl->isNewDumpAvail()) {
