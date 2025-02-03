1cbec71a0c87b55809f2882fde7706b45af351bc
Kirill Shmakov <k0ldbl00d@gmail.com>
2025-02-03 13:19:56 +0400

    Проверка срока истечения цифровой подписи

--- src/download.php
+++ src/download.php
@@ -10,6 +10,11 @@ ini_set('default_socket_timeout', 600);
 ini_set('memory_limit', '512M');
 ini_set('user_agent','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36');
 
+if($err = signExpired()) {
+    logw($err);
+    tgsend($err);
+}
+
 $dl = new Downloader();
 
 if($dl->isNewDumpAvail()) {
--- src/util.php
+++ src/util.php
@@ -4,3 +4,16 @@ function logw($message) {
     $d = date('d.m.Y H:i:s');
     echo "{$d}: {$message}\n";
 }
+
+function signExpired() {
+    $res = trim(shell_exec("openssl cms -noverify -cmsout -noout -print -inform DER -in /app/data/request.xml.p7s | grep notAfter"));
+    if(preg_match('/notAfter: (.+)/',$res,$m)) {
+        $time = strtotime($m[1]);
+        logw("Срок окончания действия цифровой подписи: " . date("d.m.Y, H:i",$time));
+        if($time - time() < 864000) {
+            return "Внимание!!! До истечения цифровой подписи осталось менее 10 суток!";
+        }
+        return false;
+    }
+    return "Внимание!!! Ошибка при проверке файла цифровой подписи. Возможные причины: некорректный файл подписи.";
+}
