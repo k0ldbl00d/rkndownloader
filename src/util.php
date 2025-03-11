adc33a8a31086989cd79eb0625d4524975b77116
Kirill Shmakov <k0ldbl00d@gmail.com>
2025-03-11 12:09:18 +0400

    Изменен способ проверки даты действия подписи. Теперь берётся дата из файла запроса.

--- src/util.php
+++ src/util.php
@@ -6,14 +6,23 @@ function logw($message) {
 }
 
 function signExpired() {
-    $res = trim(shell_exec("openssl cms -noverify -cmsout -noout -print -inform DER -in /app/data/request.xml.p7s | grep notAfter"));
-    if(preg_match('/notAfter: (.+)/',$res,$m)) {
-        $time = strtotime($m[1]);
+    $xml = file_get_contents("/app/data/request.xml");
+    if(!$xml) return "Невозможно прочесть файл запроса (request.xml)";
+    if(preg_match("/\<requestTime\>([0-9]{4}-[0-9]{2}-[0-9]{2})T([0-9]{2}:[0-9]{2}:[0-9]{2})\.([0-9]{1,4})((\+|\-)[0-9]{2}:[0-9]{2})\<\/requestTime\>/i", $xml, $m)) {
+        $time = strtotime($m[1].' '.$m[2]);
         logw("Срок окончания действия цифровой подписи: " . date("d.m.Y, H:i",$time));
-        if($time - time() < 864000) {
-            return "Внимание!!! До истечения цифровой подписи осталось менее 10 суток!";
+        $tdiff = $time - time();
+        if($tdiff < 864000) {
+            $diffdays = floor($tdiff/86400);
+            if($diffdays>0)
+                return "ВНИМАНИЕ!!! До истечения цифровой подписи осталось менее {$diffdays} суток!";
+            if($diffdays==0)
+                return "ВНИМАНИЕ!!! Цифровая подпись истекает сегодня!";
+            if($diffdays<0)
+                $diffdays = abs($diffdays);
+                return "ВНИМАНИЕ!!! Цифровая подпись истекла {$diffdays} дней назад!";
         }
         return false;
     }
-    return "Внимание!!! Ошибка при проверке файла цифровой подписи. Возможные причины: некорректный файл подписи.";
+    return "Внимание!!! Ошибка при проверке файла запроса. Возможные причины: некорректный файл запроса.";
 }
