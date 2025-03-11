80547fca2ef1e24ef17e53399fa6e1262046e16c
Kirill Shmakov <k0ldbl00d@gmail.com>
2025-03-11 14:04:18 +0400

    Исправлены сообщения об ошибках

--- src/util.php
+++ src/util.php
@@ -9,18 +9,18 @@ function signExpired() {
     $xml = file_get_contents("/app/data/request.xml");
     if(!$xml) return "Невозможно прочесть файл запроса (request.xml)";
     if(preg_match("/\<requestTime\>([0-9]{4}-[0-9]{2}-[0-9]{2})T([0-9]{2}:[0-9]{2}:[0-9]{2})\.([0-9]{1,4})((\+|\-)[0-9]{2}:[0-9]{2})\<\/requestTime\>/i", $xml, $m)) {
-        $time = strtotime($m[1].' '.$m[2]) + (365*86400);
-        logw("Срок окончания действия цифровой подписи: " . date("d.m.Y, H:i",$time));
+        $time = strtotime($m[1].' '.$m[2]) + (364*86400);
+        logw("Дата формирования файла запроса: " . date("d.m.Y, H:i",$time));
         $tdiff = $time - time();
         if($tdiff < 864000) {
             $diffdays = floor($tdiff/86400);
             if($diffdays>0)
-                return "ВНИМАНИЕ!!! До истечения цифровой подписи осталось менее {$diffdays} суток!";
+                return "ВНИМАНИЕ!!! До истечения срока действия запроса осталось {$diffdays} суток!";
             if($diffdays==0)
-                return "ВНИМАНИЕ!!! Цифровая подпись истекает сегодня!";
+                return "ВНИМАНИЕ!!! Срок действия запроса истекает сегодня!";
             if($diffdays<0)
                 $diffdays = abs($diffdays);
-                return "ВНИМАНИЕ!!! Цифровая подпись истекла {$diffdays} дней назад!";
+                return "ВНИМАНИЕ!!! Срок действия запроса истёк {$diffdays} дней назад!";
         }
         return false;
     }
